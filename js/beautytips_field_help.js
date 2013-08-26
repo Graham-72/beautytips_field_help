71d052a29692805f0a7b68f532b7dcdf2f7afe7c
Guillaume Viguier-Just <guillaume@viguierjust.com>
2013-08-26 16:54:50 +0200

    Initial commit

---
+++ js/beautytips_field_help.js
@@ -0,0 +1,10 @@
+/**
+ * Defines the default beautytip and adds them to the content on the page
+ */
+(function ($) {
+  Drupal.behaviors.beautytips_field_help = {
+    attach: function(context, settings) {
+      console.log('test');
+    }
+  }
+});
