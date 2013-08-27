bef4da528c68332f72b35dfc29d8fb49fdf3579a
Guillaume Viguier-Just <guillaume@viguierjust.com>
2013-08-27 15:19:38 +0200

    Making module work

--- js/beautytips_field_help.js
+++ js/beautytips_field_help.js
@@ -3,8 +3,16 @@
  */
 (function ($) {
   Drupal.behaviors.beautytips_field_help = {
-    attach: function(context, settings) {
-      console.log('test');
+    attach: function (context, settings) {
+      var style = settings.beautytips_field_help.style;
+      $('.form-item').each(function() {
+        if ($(this).children('div.description').length > 0) {
+          var desc = $('div.description', this).first().html();
+          $(this).children('label').first().append('<img src="/misc/help.png" alt="Help" />');
+          $(this).children('label').first().children('img').first().bt(desc, style);
+          $('div.description', this).hide();
+        }
+      });
     }
   }
-});
+})(jQuery);
